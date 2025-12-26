ac719801821867aea96f13e9b95c601e10992ddf
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-26 06:12:31 +0100

    feat: init project with dependencies and user.proto definition

---
+++ src/main/java/ma/lahjaily/ClientGrpcApplication.java
@@ -0,0 +1,13 @@
+package ma.lahjaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientGrpcApplication.class, args);
+	}
+
+}
