90b691a90f0ccbe98740ae32eababa0470573ff9
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-26 07:15:47 +0100

    feat: add Utilisateur entity and JPA repository

---
+++ src/main/java/app/grpc/entity/Utilisateur.java
@@ -0,0 +1,13 @@
+package app.grpc.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Utilisateur {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
